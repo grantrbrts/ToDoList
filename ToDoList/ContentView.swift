0278352f429cb80b19f82b5f27ece904fb66db9f
Grant Roberts <groberts96@gmail.com>
2019-11-11 07:37:55 -0800

    Working checkmarks, hardcoded todo list with strangerthings references

---
+++ ToDoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Grant Roberts on 11/10/19.
+//  Copyright © 2019 Grant Roberts. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
